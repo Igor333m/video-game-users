71f2feb3aa31cf43120274e374ddad24c1bee02d
Igor Todorovic <igor333m@yahoo.com>
2019-03-03 20:59:52 -0800

    feat: Add suer form data to state

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -2,22 +2,60 @@ import React, { Component } from 'react';
 
 class CreateUser extends Component {
 
+  state = {
+    firstName: null,
+    lastName: null,
+    userName: null
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+console.log("target: ", target);
+console.log("value: ", value);
+
+  if (!name) {  
+console.log("name: ", name);
+    }else {
+      this.setState({
+        [name]: value
+      }); 
+    }
+  }
+
+  addUser = event => {
+    event.preventDefault();
+console.log('this.state: ', this.state);
+  }
+
   render() {
     return (
       <form>
         <label>
           First Name:
-          <input type="text" name="name" />
+          <input type="text"
+            name="firstName"
+            required
+            onChange={this.handleInputChange} />
         </label>
         <label>
           Last Name:
-          <input type="text" name="name" />
+          <input type="text"
+            name="lastName"
+            required
+            onChange={this.handleInputChange} />
         </label>
         <label>
           Username:
-          <input type="text" name="name" />
+          <input type="text"
+            name="userName"
+            required
+            onChange={this.handleInputChange} />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit"
+          value="Add User"
+          onClick={this.addUser}/>
       </form>
     );
   }
